351b6e39c1994ff80a4db2ab26b94593c314d6d8
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2022-03-24 17:31:54 +0530

    feat: add suspense component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import "./App.scss";
 import { Canvas } from "@react-three/fiber";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { OrbitControls } from "@react-three/drei";
 import Earth from "./components/Earth";
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     const container = document.querySelector(".container");
     const colors = ["#fff", "#FC6400", "#0949F0"];
     for (let i = 0; i < 250; i++) {
-        const particle = document.createElement("div");
-        const bgColor = colors[Math.floor(Math.random() * colors.length)];
-        particle.classList.add("particle");
-        container.appendChild(particle);
-        particle.style.top = `${Math.random() * container.offsetHeight - 5}px`;
-        particle.style.left = `${Math.random() * container.offsetWidth - 10}px`;
-        particle.style.backgroundColor = bgColor;
-        particle.style.boxShadow = `0 0 10px ${bgColor},0 0 20px ${bgColor},0 0 40px ${bgColor}`;
-        particle.style.animationDelay = `${i / 100}s`;
-      }
-  },[])
+      const particle = document.createElement("div");
+      const bgColor = colors[Math.floor(Math.random() * colors.length)];
+      particle.classList.add("particle");
+      container.appendChild(particle);
+      particle.style.top = `${Math.random() * container.offsetHeight - 5}px`;
+      particle.style.left = `${Math.random() * container.offsetWidth - 10}px`;
+      particle.style.backgroundColor = bgColor;
+      particle.style.boxShadow = `0 0 10px ${bgColor},0 0 20px ${bgColor},0 0 40px ${bgColor}`;
+      particle.style.animationDelay = `${i / 100}s`;
+    }
+  }, []);
   return (
     <>
-      <div className="container">
-        <Canvas colorManagement camera={{ position: [-5, 2, 10], fav: 60 }}>
-          <ambientLight intensity={0.3} />
-          <directionalLight position={[0, 10, 0]} intensity={0.5} />
-          <pointLight position={[-10, 0, 20]} intensity={0.5} />
-          <pointLight position={[0, -10, 0]} intensity={1.5} />
-          <Earth />
-          <OrbitControls />
-        </Canvas>
-        <h2>Earth</h2>
-      </div>
+      <Suspense fallback={<h1>Loading theme...</h1>}>
+        <div className="container">
+          <Canvas colorManagement camera={{ position: [-5, 2, 10], fav: 60 }}>
+            <ambientLight intensity={0.3} />
+            <directionalLight position={[0, 10, 0]} intensity={0.5} />
+            <pointLight position={[-10, 0, 20]} intensity={0.5} />
+            <pointLight position={[0, -10, 0]} intensity={1.5} />
+            <Earth />
+            <OrbitControls />
+          </Canvas>
+          <h2>Earth</h2>
+        </div>
+      </Suspense>
     </>
   );
 }
